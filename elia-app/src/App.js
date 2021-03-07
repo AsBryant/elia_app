ce56f1835a6cdf8f40d679e36c8eebbb677de067
Aaron Bryant <asbryant@uclan.ac.uk>
2021-03-07 13:03:24 +0000

    Bootstrap added front page started

--- elia-app/src/App.js
+++ elia-app/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button'
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Jumbotron from "react-bootstrap/Jumbotron";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Jumbotron>
+                    <h1>Welcome to E.L.I.A!</h1>
+                    <h2>
+                        Electronic Learning Interation Assistant
+                    </h2>
+                    <p>
+                        E.L.I.A is designed to bring learning into the 21st century by providing a fun an interactive learning method.
+                        that teaches not only skills from the classroom but aims to teach social skills and fullfil the childs human interaction requirements
+                        that are reduced by a digital learning envirment
+                    </p>
+                    <ButtonGroup aria-label="Basic example">
+                        <Button size="lg" variant="outline-success" href="/login" rel="noopener noreferrer">
+                            Login here
+                        </Button>
+
+                        <Button size="lg"  href="/register" variant="outline-warning" target="_blank" rel="noopener noreferrer">
+                            Register here
+                        </Button>
+                    </ButtonGroup>
+                </Jumbotron>
+
+
+
+
+
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
